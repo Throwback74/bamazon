bc92aadde5fc89477b586d8f64cead25226bcdf9
Throwback74 <corey.slade@gmail.com>
2018-07-16 16:02:00 -0700

    going home for day

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,7 +7,7 @@ var item = [];
 var category = [];
 var price = [];
 var quantity = [];
-
+var count = 0;
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -27,33 +27,39 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     // postItem();
+    console.log("All Items available...\n");
     readItems();
 });
 
-function readItems() {
-    console.log("All Items available...\n");
+var readItems = function () {
+    // console.log("All Items available...\n");
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
-        for (var i = 0; i < res.length; i++) {
-            item_ID.push(res[i].item_id);
-            item.push(res[i].product_name);
-            category.push(res[i].department_name);
-            price.push(res[i].price);
-            quantity.push(res[i].stock_quantity);
+        // for (var i = 0; i < res.length; i++) {
+        if (count < res.length) {
+            item_ID.push(res[count].item_id);
+            item.push(res[count].product_name);
+            category.push(res[count].department_name);
+            price.push(res[count].price);
+            quantity.push(res[count].stock_quantity);
             // connection.end();
-        }
-        // console.log(item_ID, item, category, price, quantity);
+
+            console.log(item_ID, item, category, price, quantity);
             // console.log(item_ID);
-            // console.table([{
-            //     itemID: item_ID,
-            //     item: item,
-            //     category: category,
-            //     price: price,
-            //     quantity: quantity
-            // }]);
-            var itemTable = [{ ItemID: item_ID, Items: item, Category: category, Price: price, Quantity: quantity}];
-            console.table(itemTable);
-        
+            console.table([{
+                itemID: item_ID,
+                item: item,
+                category: category,
+                price: price,
+                quantity: quantity
+            }]);
+            count++;
+            readItems();
+            // var itemTable = [{ ItemID: item_ID, Items: item, Category: category, Price: price, Quantity: quantity}];
+            // console.table(itemTable);
+        } else {
+            (console.log("Still broken"));
+        }
     });
-}
+};
