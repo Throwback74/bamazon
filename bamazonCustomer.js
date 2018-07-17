4bcfdcc030d50e98d12ce8940d2334607fc7f4e7
Throwback74 <corey.slade@gmail.com>
2018-07-17 14:53:16 -0700

    update working Challenge 1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,101 +28,63 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    // postItem();
     console.log("All Items available...\n");
     readItems();
-    // start();
 });
 
 var readItems = function () {
-    // console.log("All Items available...\n");
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        // Log all results of the SELECT statement
-        // for (var i = 0; i < res.length; i++) {
-        if (count < res.length) {
-            item_ID.push(res[count].item_id);
-            item.push(res[count].product_name);
-            category.push(res[count].department_name);
-            price.push(res[count].price);
-            quantity.push(res[count].stock_quantity);
-
-            // connection.end();
-
-            // console.log(item_ID, item, category, price, quantity);
-            // console.log(item_ID);
-            console.table([{
-                itemID: item_ID[count],
-                item: item[count],
-                category: category[count],
-                price: price[count],
-                quantity: quantity[count]
-            }]);
+            if (count < res.length) {
+                item_ID.push(JSON.stringify(res[count].item_id));
+                item.push(res[count].product_name);
+                category.push(res[count].department_name);
+                price.push(res[count].price);
+                quantity.push(res[count].stock_quantity);
             count++;
             readItems();
-            // var itemTable = [{ ItemID: item_ID, Items: item, Category: category, Price: price, Quantity: quantity}];
-            // console.table(itemTable);
+            
         } else {
+            console.table(res);
             console.log(item_ID);
             (console.log("...End List"));
-            // start();
-            // connection.end();
-        }
-    // }).then(function (initiate) {
-    //     start();
+            promptID(res);
+            }
     });
 };
 
-inquirer.prompt([{
-    name: "selectPurchase",
-    type: "list",
-    message: "Select the ItemID for the item you would like to purchase",
-    choices: item_ID
-}, {
-    name: "purchaseQuantity",
-    type: "input",
-    message: "How many would you like?"
-}]).then(function (purchase) {
-    itemPurchased = parseInt(purchase.selectPurchase);
-    quantityBought = parseInt(purchase.purchaseQuantity);
-    console.log('Buying: ' + quantityBought + ' of ' + itemPurchased);
-    // if (itemPurchased < 11) {
-    //     purchaseItem(itemPurchased, quantityBought);
-    // } else {
-    //     console.log("Please select an item ID");
-    // }
-},function (error) {
-    console.error('uh oh: ', error);   // 'uh oh: something bad happened’
-});
 
-// var start = function () {
-//     connection.query("SELECT * FROM products", function (err, res) {
-//         if (err) throw err;
-//         inquirer.prompt({
-//             name: "selectPurchase",
-//             type: "list",
-//             message: "Select the ItemID for the item you would like to purchase",
-//             choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-//         }, {
-//             name: "purchaseQuantity",
-//             type: "input",
-//             message: "How many would you like?"
-//         }).then(function (answer) {
-//             itemPurchased = parseInt(answer.selectPurchase);
-//             quantityBought = parseInt(answer.purchaseQuantity);
-//             if (itemPurchased < 11) {
-//                 purchaseItem(itemPurchased, quantityBought);
-//             } else {
-//                 console.log("Please select an item ID");
-//             }
-//         });
-//     });
-// };
+var promptID = function (res) {
+    inquirer.prompt([{
+            name: "selectPurchase",
+            type: "list",
+            message: "Select the ItemID for the item you would like to purchase",
+            choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] // item_ID
+        }, {
+            name: "purchaseQuantity",
+            type: "input",
+            message: "How many would you like?"
+        }]).then(function (purchase) {
+            itemPurchased = parseInt(purchase.selectPurchase);
+            quantityBought = parseInt(purchase.purchaseQuantity);
+            console.log('Buying: ' + quantityBought + " " + item[itemPurchased-1]); //fix line to ref item name
+            if (itemPurchased < 11) {
+                purchaseItem(itemPurchased, quantityBought, res);
+            } else {
+                console.log("Please select an item ID");
+            }
+            
+        }, function (error) {
+            console.error('uh oh: ', error); // 'uh oh: something bad happened’
+        });
+};
 
-var purchaseItem = function (itemChoice, quantityPurchased) {
-    if (quantityPurchased <= quantity[itemChoice]) {
+var purchaseItem = function (itemChoice, quantityPurchased, res) {
+    var itemChoiceIndex = (itemChoice-1);
+    
+    if (quantityPurchased <= quantity[itemChoiceIndex]) {
         console.log("Processing Purchase...\n");
-        quantity = quantity[itemChoice] - quantityPurchased;
+        quantity = quantity[itemChoiceIndex] - quantityPurchased;
         var query = connection.query(
             "UPDATE products SET ? WHERE ?", [{
                     stock_quantity: quantity
@@ -132,8 +94,8 @@ var purchaseItem = function (itemChoice, quantityPurchased) {
                 }
             ],
             function (err, res) {
-                console.log(item[itemChoice] + " Purchased!\n");
-                console.log('Total: ' + (price[itemChoice] * quantity));
+                console.log(item[itemChoiceIndex] + " Purchased!\n");
+                console.log('Total: $' + (price[itemChoiceIndex] * quantityPurchased));
             }
         );
 
@@ -143,4 +105,5 @@ var purchaseItem = function (itemChoice, quantityPurchased) {
     } else {
         console.log('Insufficient Quantity');
     }
+    connection.end();
 };
